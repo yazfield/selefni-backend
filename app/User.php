d62dd9225c3aa95e9fe3cf04b8500f7f5e2abd6b
yazfield <hanifi.yazid@gmail.com>
2016-12-30 15:04:44 +0100

    Added active scope to User model

--- app/User.php
+++ app/User.php
@@ -2,28 +2,66 @@
 
 namespace App;
 
+use Illuminate\Contracts\Hashing\Hasher;
+use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 
+/**
+ * Class User model
+ * @package App
+ */
 class User extends Authenticatable
 {
-    use Notifiable;
+    use Notifiable, SoftDeletes;
 
     /**
      * The attributes that are mass assignable.
      *
      * @var array
      */
-    protected $fillable = [
-        'name', 'email', 'password',
-    ];
+    protected $fillable = ['name', 'email', 'password', 'phone_number', 'active',];
 
     /**
      * The attributes that should be hidden for arrays.
      *
      * @var array
      */
-    protected $hidden = [
-        'password', 'remember_token',
-    ];
+    protected $hidden = ['password', 'remember_token', 'deleted_at',];
+
+    /**
+     * Cast to Carbon dates
+     * @var array
+     */
+    protected $dates = ['deleted_at'];
+
+    /**
+     * Casting fields to php types
+     * @var array
+     */
+    protected $casts = ['active' => 'boolean',];
+
+    /**
+     * User constructor.
+     * @param array $attributes
+     * @param Hasher $hasher
+     */
+    public function __construct(array $attributes = [], Hasher $hasher)
+    {
+        parent::__construct($attributes);
+        $this->hasher = $hasher;
+    }
+
+    /**
+     * Hashing password value before saving
+     * @param String $value Password
+     */
+    public function setPasswordAttribute($value)
+    {
+        $this->attributes['password'] = $this->hasher->make($value);
+    }
+
+    public function scopeActive($query) {
+        return $query->where('active', true);
+    }
 }
