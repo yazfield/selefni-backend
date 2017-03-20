c167390ee22969c94ced60e15c999810939037c7
yazfield <hanifi.yazid@gmail.com>
2017-03-20 18:14:37 +0100

    L 5.4 migration

---
+++ tests/BrowserKitTest.php
@@ -0,0 +1,26 @@
+<?php
+namespace Tests;
+
+abstract class BrowserKitTest extends \Laravel\BrowserKitTesting\TestCase
+{
+    /**
+     * The base URL to use while testing the application.
+     *
+     * @var string
+     */
+    protected $baseUrl = 'http://localhost';
+
+    /**
+     * Creates the application.
+     *
+     * @return \Illuminate\Foundation\Application
+     */
+    public function createApplication()
+    {
+        $app = require __DIR__.'/../bootstrap/app.php';
+
+        $app->make(\Illuminate\Contracts\Console\Kernel::class)->bootstrap();
+
+        return $app;
+    }
+}
