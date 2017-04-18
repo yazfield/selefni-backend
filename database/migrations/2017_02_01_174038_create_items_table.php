0308da2df191babad32a91d6033a09ef7796683d
yazfield <hanifi.yazid@gmail.com>
2017-04-18 15:14:18 +0100

    added fields to items table

--- database/migrations/2017_02_01_174038_create_items_table.php
+++ database/migrations/2017_02_01_174038_create_items_table.php
@@ -1,8 +1,8 @@
 <?php
 
-use Illuminate\Support\Facades\Schema;
-use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
 
 class CreateItemsTable extends Migration
 {
@@ -22,6 +22,8 @@ class CreateItemsTable extends Migration
             $table->timestamp('return_at');
             $table->integer('borrowed_to');
             $table->integer('borrowed_from');
+            $table->timestamp('returned_at')->nullable();
+            $table->timestamp('borrowed_at')->useCurrent();
             $table->softDeletes();
             $table->timestamps();
         });
