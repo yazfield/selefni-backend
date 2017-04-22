edbe000ec82885bac794fc0d4104a45ac21a80be
yazfield <hanifi.yazid@gmail.com>
2017-04-23 00:47:54 +0100

    Added i18n lang

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -5,21 +5,25 @@ import {sync} from "vuex-router-sync";
 import axios from "axios";
 import VueMaterial from "vue-material";
 import infiniteScroll from "vue-infinite-scroll";
+import VueI18n from "vue-i18n";
 import AppTheme from "../sass/AppTheme";
 import App from "./components/App";
 import {baseDomain} from "./constants";
 import store from "./store";
 import {router} from "./router";
+import SlideTransition from "./components/BasicSlideTransition";
+import locales from "./locales";
 
 Vue.use(Vuex);
 Vue.use(VueRouter);
 Vue.use(VueMaterial);
 Vue.use(infiniteScroll);
-
+Vue.use(VueI18n);
 
 Vue.material.registerTheme('default', AppTheme);
 
 Vue.component('App', App);
+Vue.component('slide-transition', SlideTransition);
 
 axios.defaults.baseUrl = baseDomain;
 Vue.prototype.$http = axios;
@@ -38,8 +42,15 @@ axios.interceptors.request.use(function (config) {
 // FIXME: not sure if useful
 sync(store, router);
 
+const i18n = new VueI18n({
+    locale: 'en', // set locale
+    fallbackLocale: 'en',
+    messages: locales, // set locale messages
+});
+
 const app = new Vue({
     el: '#app',
     store,
-    router
+    router,
+    i18n
 });
---
+++ resources/assets/js/locales.js
@@ -0,0 +1,31 @@
+export default {
+    en: {
+        item: {
+            notification: {
+                notify: 'Notify'
+            },
+            friend: {
+                borrowed_from: 'Borrowed from',
+                borrowed_to: 'Borrowed to',
+                direction: 'Direction' // from or to?
+            },
+            search: 'Search',
+            details: 'Details',
+            amount: {
+                header: 'More information',
+                label: 'Amount'
+            },
+            name: {
+                label: 'Give me a name'
+            },
+            type: {
+                label: 'Item type',
+                types: {
+                    object: 'Object',
+                    money: 'Money',
+                    book: 'Book'
+                }
+            }
+        }
+    }
+};
